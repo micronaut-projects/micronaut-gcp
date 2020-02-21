c5eda8e77637ead4d602f3240f0e5e530747226e
graemerocher <graeme.rocher@gmail.com>
2020-02-21 13:18:56 +0100

    Cleanup

--- gcp-function-http-server/src/main/java/io/micronaut/gcp/function/http/GoogleFunctionHttpRequest.java
+++ gcp-function-http-server/src/main/java/io/micronaut/gcp/function/http/GoogleFunctionHttpRequest.java
@@ -33,7 +33,6 @@ import java.util.*;
  */
 @Internal
 final class GoogleFunctionHttpRequest<B> implements ServerlessHttpRequest<com.google.cloud.functions.HttpRequest, B>, ServerlessExchange<com.google.cloud.functions.HttpRequest, com.google.cloud.functions.HttpResponse> {
-    private static final String HEADER_KEY_VALUE_SEPARATOR = "=";
     private final com.google.cloud.functions.HttpRequest googleRequest;
     private final URI uri;
     private final HttpMethod method;
@@ -242,79 +241,6 @@ final class GoogleFunctionHttpRequest<B> implements ServerlessHttpRequest<com.go
         return s.replaceAll("[\r\n]", "");
     }
 
-    private static List<HeaderValue> parseHeaderValue(
-            String headerValue, String valueSeparator, String qualifierSeparator) {
-        // Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8
-        // Accept-Language: fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5
-        // Cookie: name=value; name2=value2; name3=value3
-        // X-Custom-Header: YQ==
-
-        List<HeaderValue> values = new ArrayList<>();
-        if (headerValue == null) {
-            return values;
-        }
-
-        for (String v : headerValue.split(valueSeparator)) {
-            String curValue = v;
-            float curPreference = 1.0f;
-            HeaderValue newValue = new HeaderValue();
-            newValue.setRawValue(v);
-
-            for (String q : curValue.split(qualifierSeparator)) {
-
-                String[] kv = q.split(HEADER_KEY_VALUE_SEPARATOR, 2);
-                String key = null;
-                String val = null;
-                // no separator, set the value only
-                if (kv.length == 1) {
-                    val = q.trim();
-                }
-                // we have a separator
-                if (kv.length == 2) {
-                    // if the length of the value is 0 we assume that we are looking at a
-                    // base64 encoded value with padding so we just set the value. This is because
-                    // we assume that empty values in a key/value pair will contain at least a white space
-                    if (kv[1].length() == 0) {
-                        val = q.trim();
-                    }
-                    // this was a base64 string with an additional = for padding, set the value only
-                    if ("=".equals(kv[1].trim())) {
-                        val = q.trim();
-                    } else { // it's a proper key/value set both
-                        key = kv[0].trim();
-                        val = ("".equals(kv[1].trim()) ? null : kv[1].trim());
-                    }
-                }
-
-                if (newValue.getValue() == null) {
-                    newValue.setKey(key);
-                    newValue.setValue(val);
-                } else {
-                    // special case for quality q=
-                    if ("q".equals(key)) {
-                        curPreference = Float.parseFloat(val);
-                    } else {
-                        newValue.addAttribute(key, val);
-                    }
-                }
-            }
-            newValue.setPriority(curPreference);
-            values.add(newValue);
-        }
-
-        // sort list by preference
-        values.sort((HeaderValue first, HeaderValue second) -> {
-            if ((first.getPriority() - second.getPriority()) < .001f) {
-                return 0;
-            }
-            if (first.getPriority() < second.getPriority()) {
-                return 1;
-            }
-            return -1;
-        });
-        return values;
-    }
-
     /**
      * Models the http parameters.
      */
@@ -342,69 +268,6 @@ final class GoogleFunctionHttpRequest<B> implements ServerlessHttpRequest<com.go
     private final class GoogleFunctionHeaders extends GoogleMultiValueMap implements HttpHeaders {
         GoogleFunctionHeaders() {
             super(googleRequest.getHeaders());
-        }
-    }
-
-    /**
-     * Class that represents a header value.
-     */
-    private static class HeaderValue {
-        private String key;
-        private String value;
-        private String rawValue;
-        private float priority;
-        private Map<String, String> attributes;
-
-        public HeaderValue() {
-            attributes = new HashMap<>();
-        }
-
-        public String getKey() {
-            return key;
-        }
-
-        public void setKey(String key) {
-            this.key = key;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        public void setValue(String value) {
-            this.value = value;
-        }
-
-        public String getRawValue() {
-            return rawValue;
-        }
-
-        public void setRawValue(String rawValue) {
-            this.rawValue = rawValue;
-        }
-
-        public float getPriority() {
-            return priority;
-        }
-
-        public void setPriority(float priority) {
-            this.priority = priority;
-        }
-
-        public Map<String, String> getAttributes() {
-            return attributes;
-        }
-
-        public void setAttributes(Map<String, String> attributes) {
-            this.attributes = attributes;
-        }
-
-        public void addAttribute(String key, String value) {
-            attributes.put(key, value);
-        }
-
-        public String getAttribute(String key) {
-            return attributes.get(key);
         }
     }
 }
