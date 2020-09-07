808d1035d461d6b50f1b9c2416395b7788efbc60
Graeme Rocher <graeme.rocher@oracle.com>
2020-09-07 16:13:05 +0200

    add post test

--- gcp-function-http-test/src/test/groovy/io/micronaut/gcp/function/http/test/InvokerHttpServerSpec.groovy
+++ gcp-function-http-test/src/test/groovy/io/micronaut/gcp/function/http/test/InvokerHttpServerSpec.groovy
@@ -1,8 +1,11 @@
 package io.micronaut.gcp.function.http.test
 
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.annotation.MicronautTest
@@ -25,12 +28,26 @@ class InvokerHttpServerSpec extends Specification {
         result == 'good'
     }
 
+    void 'test invoke post via server'() {
+        when:
+        def result = client.retrieve(HttpRequest.POST('/test', "body")
+                .contentType(MediaType.TEXT_PLAIN), String).blockingFirst()
+
+        then:
+        result == 'goodbody'
+    }
+
 
     @Controller('/test')
     static class TestController {
         @Get(value = '/', produces = MediaType.TEXT_PLAIN)
         String test() {
             return 'good'
+        }
+
+        @Post(value = '/', processes = MediaType.TEXT_PLAIN)
+        String test(@Body String body) {
+            return 'good' + body
         }
     }
 }
