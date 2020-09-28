e9303d5979524eaf9dfa3367072632b61d379168
Iván López <lopezi@objectcomputing.com>
2020-09-28 14:00:13 +0200

    Only create mocked credentials for PubSubEmulator test

--- gcp-pubsub/src/test/groovy/io/micronaut/gcp/pubsub/configuration/PubSubEmulatorSpec.groovy
+++ gcp-pubsub/src/test/groovy/io/micronaut/gcp/pubsub/configuration/PubSubEmulatorSpec.groovy
@@ -11,36 +11,49 @@ import com.google.auth.oauth2.GoogleCredentials
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Replaces
+import io.micronaut.context.annotation.Requires
 import io.micronaut.gcp.Modules
 import io.micronaut.inject.qualifiers.Qualifiers
 import spock.lang.Specification
-import spock.mock.MockingApi
 
 import javax.inject.Singleton
 
 class PubSubEmulatorSpec extends Specification{
 
     void "test environment without pubsub emulator"() {
-        ApplicationContext ctx = ApplicationContext.run()
-        CredentialsProvider provider = ctx.getBean(CredentialsProvider, Qualifiers.byName(Modules.PUBSUB))
-        TransportChannelProvider transportChannelProvider = ctx.getBean(TransportChannelProvider, Qualifiers.byName(Modules.PUBSUB))
-        expect:
-            provider instanceof FixedCredentialsProvider
-            transportChannelProvider instanceof InstantiatingGrpcChannelProvider
-    }
-    void "test environment with pubsub emulator"() {
+        given:
         ApplicationContext ctx = ApplicationContext.run([
-                "pubsub.emulator.host" : "localhost:8085"])
+            'spec.name': 'PubSubEmulatorSpec'
+        ])
+
+        when:
         CredentialsProvider provider = ctx.getBean(CredentialsProvider, Qualifiers.byName(Modules.PUBSUB))
         TransportChannelProvider transportChannelProvider = ctx.getBean(TransportChannelProvider, Qualifiers.byName(Modules.PUBSUB))
 
-        expect:
-            provider instanceof NoCredentialsProvider
-            transportChannelProvider instanceof FixedTransportChannelProvider
+        then:
+        provider instanceof FixedCredentialsProvider
+        transportChannelProvider instanceof InstantiatingGrpcChannelProvider
+    }
+
+    void "test environment with pubsub emulator"() {
+        given:
+        ApplicationContext ctx = ApplicationContext.run([
+            'spec.name': 'PubSubEmulatorSpec',
+            "pubsub.emulator.host" : "localhost:8085"
+        ])
+
+        when:
+        CredentialsProvider provider = ctx.getBean(CredentialsProvider, Qualifiers.byName(Modules.PUBSUB))
+        TransportChannelProvider transportChannelProvider = ctx.getBean(TransportChannelProvider, Qualifiers.byName(Modules.PUBSUB))
+
+        then:
+        provider instanceof NoCredentialsProvider
+        transportChannelProvider instanceof FixedTransportChannelProvider
     }
 }
 
 @Factory
+@Requires(property = 'spec.name', value = 'PubSubEmulatorSpec')
 class EmptyCredentialsFactory {
 
     @Singleton
