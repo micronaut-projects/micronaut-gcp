44d634055bffeeb1fd67a8ffdc49b86e60e0ff78
Álvaro Sánchez-Mariscal <alvarosanchez@users.noreply.github.com>
2020-12-10 11:49:58 +0100

    Since 3.4.0

--- gcp-secret-manager/src/main/java/io/micronaut/gcp/secretmanager/configuration/SecretManagerConfigurationProperties.java
+++ gcp-secret-manager/src/main/java/io/micronaut/gcp/secretmanager/configuration/SecretManagerConfigurationProperties.java
@@ -26,7 +26,7 @@ import java.util.Set;
 /**
  * Configuration for SecretManager clients and config client integration.
  * @author Vinicius Carvalho
- * @since 3.2.0
+ * @since 3.4.0
  */
 @ConfigurationProperties(SecretManagerConfigurationProperties.PREFIX)
 @BootstrapContextCompatible
