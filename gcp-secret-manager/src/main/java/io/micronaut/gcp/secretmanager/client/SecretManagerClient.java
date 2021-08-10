bbeece37878d0985055b9230452c68200fafe02b
jameskleeh <james.kleeh@gmail.com>
2021-08-10 11:33:16 -0400

    Use RC3 of servlet. Fix status changes not being reflected in getStatus()

--- gcp-secret-manager/src/main/java/io/micronaut/gcp/secretmanager/client/SecretManagerClient.java
+++ gcp-secret-manager/src/main/java/io/micronaut/gcp/secretmanager/client/SecretManagerClient.java
@@ -17,7 +17,6 @@ package io.micronaut.gcp.secretmanager.client;
 
 import io.micronaut.core.async.annotation.SingleResult;
 import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
 
 /**
  * This interface is intended to abstract interactions with {@link com.google.cloud.secretmanager.v1.SecretManagerServiceClient}, and instead of returning Google's {@link com.google.api.core.ApiFuture}
