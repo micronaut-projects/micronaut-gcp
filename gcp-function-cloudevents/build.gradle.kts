45f2d47824d5a848c5ab941eef4a58e222a0be1c
Sergio del Amo <sergio.delamo@softamo.com>
2022-11-24 07:18:37 +0100

    use version catalogues

--- gcp-function-cloudevents/build.gradle.kts
+++ gcp-function-cloudevents/build.gradle.kts
@@ -5,8 +5,8 @@ dependencies {
     api(libs.managed.google.cloudevent.types)
     api(libs.managed.functions.framework.api)
     api(project(":gcp-function"))
-    annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
-    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    annotationProcessor(mn.micronaut.serde.processor)
+    implementation(mn.micronaut.serde.jackson)
     testImplementation(libs.cloudevents.core)
 }
 
