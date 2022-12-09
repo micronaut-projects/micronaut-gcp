2d2daed3caeeafd152b202ee5e4ac674fa5ed794
Dean Wette <wetted@objectcomputing.com>
2022-12-09 09:06:02 -0600

    fix misc check errors

--- gcp-secret-manager/src/main/java/io/micronaut/gcp/secretmanager/client/DefaultSecretManagerClient.java
+++ gcp-secret-manager/src/main/java/io/micronaut/gcp/secretmanager/client/DefaultSecretManagerClient.java
@@ -15,23 +15,24 @@
  */
 package io.micronaut.gcp.secretmanager.client;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import com.google.api.core.ApiFuture;
-import com.google.cloud.secretmanager.v1.*;
+import com.google.cloud.secretmanager.v1.AccessSecretVersionRequest;
+import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
+import com.google.cloud.secretmanager.v1.SecretVersionName;
 import io.micronaut.context.annotation.BootstrapContextCompatible;
 import io.micronaut.context.annotation.Requires;
-import io.micronaut.context.env.Environment;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.gcp.GoogleCloudConfiguration;
 import io.micronaut.scheduling.TaskExecutors;
 import jakarta.inject.Named;
+import jakarta.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import jakarta.inject.Singleton;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Default implementation of {@link SecretManagerClient}.
@@ -43,7 +44,7 @@ import reactor.core.publisher.Mono;
 @Requires(classes = SecretManagerServiceClient.class)
 public class DefaultSecretManagerClient implements SecretManagerClient {
 
-    private static final Logger logger = LoggerFactory.getLogger(DefaultSecretManagerClient.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultSecretManagerClient.class);
     private final SecretManagerServiceClient client;
     private final GoogleCloudConfiguration googleCloudConfiguration;
     private final ExecutorService executorService;
@@ -69,7 +70,7 @@ public class DefaultSecretManagerClient implements SecretManagerClient {
 
     @Override
     public Mono<VersionedSecret> getSecret(String secretId, String version, String projectId) {
-        logger.debug("Fetching secret: projects/{}/secrets/{}/{}", projectId, secretId, version);
+        LOGGER.debug("Fetching secret: projects/{}/secrets/{}/{}", projectId, secretId, version);
         SecretVersionName secretVersionName = SecretVersionName.of(projectId, secretId, version);
         AccessSecretVersionRequest request = AccessSecretVersionRequest.newBuilder()
                 .setName(secretVersionName.toString())
