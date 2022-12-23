f533e93a93f32ee2944c3d118b8915923067b765
Sergio del Amo <sergio.delamo@softamo.com>
2022-12-23 08:40:53 +0100

    buidl: use gcp-module

--- gcp-function-cloudevents/build.gradle.kts
+++ gcp-function-cloudevents/build.gradle.kts
@@ -1,18 +1,11 @@
 plugins {
-    id("io.micronaut.build.internal.module")
+    id("io.micronaut.build.internal.gcp-module")
 }
 dependencies {
     api(libs.cloudevents.api)
     api(libs.managed.functions.framework.api)
-    api(project(":gcp-function"))
-    api(project(":gcp-serde-cloudevents"))
-    implementation(mn.micronaut.serde.jackson)
+    api(projects.gcpFunction)
+    api(projects.gcpSerdeCloudevents)
+    implementation(mnSerde.micronaut.serde.jackson)
     testImplementation(libs.cloudevents.core)
-}
-
-//TODO Don't pin version once this module uses MN 3.8.0
-configurations.all {
-    resolutionStrategy {
-        force("io.micronaut.serde:micronaut-serde-jackson:1.5.0")
-    }
 }
