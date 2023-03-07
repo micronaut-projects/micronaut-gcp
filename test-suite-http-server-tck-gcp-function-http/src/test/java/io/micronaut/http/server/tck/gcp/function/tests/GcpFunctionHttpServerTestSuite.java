24150dfc384427ed83dda583bb57140e93461806
Tim Yates <tim.yates@gmail.com>
2023-03-07 09:17:50 +0000

    Add comments to the TCK exclusions to aid future fixers (#789)

--- test-suite-http-server-tck-gcp-function-http/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpServerTestSuite.java
+++ test-suite-http-server-tck-gcp-function-http/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpServerTestSuite.java
@@ -1,7 +1,6 @@
 package io.micronaut.http.server.tck.gcp.function.tests;
 
 import org.junit.platform.suite.api.ExcludeClassNamePatterns;
-import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
@@ -9,16 +8,14 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for for GCP Function HTTP")
-@ExcludeClassNamePatterns(
-    {
-        "io.micronaut.http.server.tck.tests.CookiesTest",
-        "io.micronaut.http.server.tck.tests.ConsumesTest",
-        "io.micronaut.http.server.tck.tests.filter.HttpServerFilterTest",
-        "io.micronaut.http.server.tck.tests.BodyTest",
-        "io.micronaut.http.server.tck.tests.MiscTest",
-        "io.micronaut.http.server.tck.tests.OctetTest",
-        "io.micronaut.http.server.tck.tests.ErrorHandlerTest"
-    }
-)
+@ExcludeClassNamePatterns({
+    "io.micronaut.http.server.tck.tests.BodyTest", // Error decoding JSON stream for type [List|pojo]
+    "io.micronaut.http.server.tck.tests.ConsumesTest", // Unable to decode request body: Error decoding JSON stream for type [pojo]
+    "io.micronaut.http.server.tck.tests.CookiesTest", // Cookies are not handled, I suspect as it's not implemented in the NettyClientHttpRequest
+    "io.micronaut.http.server.tck.tests.ErrorHandlerTest", // Error decoding JSON stream for type [object]
+    "io.micronaut.http.server.tck.tests.MiscTest", // Decoding errors and bad requests
+    "io.micronaut.http.server.tck.tests.OctetTest", // Body is badly encoded
+    "io.micronaut.http.server.tck.tests.filter.HttpServerFilterTest", // Expected io.micronaut.http.client.exceptions.HttpClientResponseException to be thrown, but nothing was thrown.
+})
 class GcpFunctionHttpServerTestSuite {
 }
