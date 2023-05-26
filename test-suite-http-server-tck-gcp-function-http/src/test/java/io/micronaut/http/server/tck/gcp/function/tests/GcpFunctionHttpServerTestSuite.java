74620b70a5b509912cb89af627eb6594e43ecfb0
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-26 09:45:45 +0200

    remove tests moved to the http client tck

--- test-suite-http-server-tck-gcp-function-http/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpServerTestSuite.java
+++ test-suite-http-server-tck-gcp-function-http/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpServerTestSuite.java
@@ -17,8 +17,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
     "io.micronaut.http.server.tck.tests.CookiesTest", // Cookies are not handled, I suspect as it's not implemented in the NettyClientHttpRequest
     "io.micronaut.http.server.tck.tests.ErrorHandlerTest", // testCustomGlobalExceptionHandlersForPOSTWithBody fails; cannot get ErrorHandlerTest$RequestObject no String-argument constructor/factory method to deserialize from String value ('{\"numberField\":101}')
     "io.micronaut.http.server.tck.tests.MiscTest", // Bad Request 400 errors
-    "io.micronaut.http.server.tck.tests.filter.ClientRequestFilterTest", // Multiple errors, mostly 404s
-    "io.micronaut.http.server.tck.tests.filter.ClientResponseFilterTest", // Multiple errors
     "io.micronaut.http.server.tck.tests.filter.RequestFilterExceptionHandlerTest", // Request is immutable
     "io.micronaut.http.server.tck.tests.filter.RequestFilterTest", // Request is immutable
     "io.micronaut.http.server.tck.tests.filter.ResponseFilterTest", // Request is immutable
