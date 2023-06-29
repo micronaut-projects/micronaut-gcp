4f8cfadfc14be948b04be3d1693dda879cfd69cc
Tim Yates <tim.yates@gmail.com>
2023-06-29 10:06:30 +0100

    test-suite-http-server-tck-gcp-function-http-test TCK all passes

--- test-suite-http-server-tck-gcp-function-http-test/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpTestServerTestSuite.java
+++ test-suite-http-server-tck-gcp-function-http-test/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpTestServerTestSuite.java
@@ -1,6 +1,5 @@
 package io.micronaut.http.server.tck.gcp.function.tests;
 
-import org.junit.platform.suite.api.ExcludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
@@ -8,9 +7,5 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for GCP Function HTTP Test")
-@ExcludeClassNamePatterns({
-    "io.micronaut.http.server.tck.tests.filter.RequestFilterTest", // Unbindable argument [byte[] bytes] to method [void requestFilterBinding(String contentType,byte[] bytes,FilterContinuation<HttpResponse<Object>> continuation)]
-    "io.micronaut.http.server.tck.tests.constraintshandler.ControllerConstraintHandlerTest" // Broken in servlet
-})
 class GcpFunctionHttpTestServerTestSuite {
 }
