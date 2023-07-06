acd511836682f8c604e45b819f167d5ea15b3a1c
Sergio del Amo <sergio.delamo@softamo.com>
2023-07-06 21:25:14 +0200

    ignore io.micronaut.http.server.tck.tests.codec.JsonCodeAdditionalTypeTest

--- test-suite-http-server-tck-gcp-function-http-test/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpTestServerTestSuite.java
+++ test-suite-http-server-tck-gcp-function-http-test/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpTestServerTestSuite.java
@@ -1,5 +1,6 @@
 package io.micronaut.http.server.tck.gcp.function.tests;
 
+import org.junit.platform.suite.api.ExcludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
@@ -7,5 +8,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for GCP Function HTTP Test")
+@ExcludeClassNamePatterns("io.micronaut.http.server.tck.tests.codec.JsonCodeAdditionalTypeTest")
 class GcpFunctionHttpTestServerTestSuite {
 }
