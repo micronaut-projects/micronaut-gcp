06768f0c5dc150e3c60ec3976be6706a79843967
Sergio del Amo <sergio.delamo@softamo.com>
2023-07-07 11:25:30 +0200

    build: add gcp-tracing and gcp-logging modules (#887)
    Close: https://github.com/micronaut-projects/micronaut-gcp/issues/878

--- gcp-tracing/build.gradle.kts
+++ gcp-tracing/build.gradle.kts
@@ -4,16 +4,13 @@ plugins {
 
 dependencies {
     api(projects.micronautGcpCommon)
-    api(mnTracing.micronaut.tracing.zipkin)
-
+    api(mnTracing.micronaut.tracing.brave.http)
     implementation(libs.google.auth.library.credentials)
     implementation(libs.zipkin.sender.stackdriver)
     implementation(libs.brave.propagation.stackdriver)
     implementation(libs.grpc.auth)
-
     implementation(libs.grpc.netty.shaded)
     implementation(libs.brave.opentracing)
-
     testAnnotationProcessor(mn.micronaut.inject.java)
     testImplementation(mn.micronaut.inject.java)
 }
