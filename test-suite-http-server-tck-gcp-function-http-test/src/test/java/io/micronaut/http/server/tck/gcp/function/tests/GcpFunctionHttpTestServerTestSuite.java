75c358c9886e118e2378c0054c1dd5991627d9ca
Tim Yates <tim.yates@gmail.com>
2024-03-19 10:52:47 +0000

    Fix getURI instead of replacing the tests 😇

--- test-suite-http-server-tck-gcp-function-http-test/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpTestServerTestSuite.java
+++ test-suite-http-server-tck-gcp-function-http-test/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpTestServerTestSuite.java
@@ -7,14 +7,9 @@ import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
-@SelectPackages({
-    "io.micronaut.http.server.tck.tests",
-    "io.micronaut.http.server.tck.gcp.function.tests"
-})
+@SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for GCP Function HTTP Test")
 @ExcludeClassNamePatterns({
-    "io.micronaut.http.server.tck.tests.hateoas.JsonErrorTest", // Replaced by JsonErrorReplacementTest
-    "io.micronaut.http.server.tck.tests.hateoas.VndErrorTest", // Replaced by VndErrorReplacementTest
     "io.micronaut.http.server.tck.tests.LocalErrorReadingBodyTest",
     "io.micronaut.http.server.tck.tests.FilterProxyTest",
     "io.micronaut.http.server.tck.tests.filter.options.OptionsFilterTest", // https://github.com/micronaut-projects/micronaut-core/pull/10126 removes a header which is not currently supported in gcp-function-http-test
