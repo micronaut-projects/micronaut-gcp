3bfe7466e38bcd91ab88dc6eb23300de9c6e4c6d
Sergio del Amo <sergio.delamo@softamo.com>
2024-12-23 11:52:26 +0100

    force version of logback classic from micronaut logging
    logback.json.classic still has a dependency to a vulnerable dependency

--- gcp-logging/build.gradle.kts
+++ gcp-logging/build.gradle.kts
@@ -5,7 +5,10 @@ plugins {
 dependencies {
     compileOnly(projects.micronautGcpTracing)
     api(projects.micronautGcpCommon)
-    implementation(libs.logback.json.classic)
+    implementation(libs.logback.json.classic) {
+        exclude(group = "ch.qos.logback", module = "logback-classic")
+    }
+    implementation(mnLogging.logback.classic)
     implementation(mn.micronaut.json.core)
     testAnnotationProcessor(mn.micronaut.inject.java)
     testImplementation(mnTestResources.testcontainers.core)
