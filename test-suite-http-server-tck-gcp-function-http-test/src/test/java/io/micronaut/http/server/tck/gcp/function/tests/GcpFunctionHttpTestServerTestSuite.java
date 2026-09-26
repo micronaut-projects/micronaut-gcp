71041b497e58c39bf0da5828bd3a356962fe7b8f
Sergio del Amo <sergio.del.amo.caballero@oracle.com>
2026-09-26 09:06:31 +0200

    Exclude request size tests from GCP Function TCK
    Add @ExcludeTags("max-request-size") to the GCP Function HTTP and HTTP Test server TCK suites so both skip tests tagged for maximum request size checks.

--- test-suite-http-server-tck-gcp-function-http-test/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpTestServerTestSuite.java
+++ test-suite-http-server-tck-gcp-function-http-test/src/test/java/io/micronaut/http/server/tck/gcp/function/tests/GcpFunctionHttpTestServerTestSuite.java
@@ -1,11 +1,13 @@
 package io.micronaut.http.server.tck.gcp.function.tests;
 
 import org.junit.platform.suite.api.ExcludeClassNamePatterns;
+import org.junit.platform.suite.api.ExcludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
+@ExcludeTags("max-request-size")
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for GCP Function HTTP Test")
 @ExcludeClassNamePatterns({
